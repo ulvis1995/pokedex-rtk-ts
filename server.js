0b7d4e0d8cc4db5d69c40bd567498c79178b7e57
ViolettaParamonova <violettapar1995@gmail.com>
2022-09-08 18:10:11 +0300

    server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "public", "index.html")));
+app.use(express.static(path.join(__dirname, "build")));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
